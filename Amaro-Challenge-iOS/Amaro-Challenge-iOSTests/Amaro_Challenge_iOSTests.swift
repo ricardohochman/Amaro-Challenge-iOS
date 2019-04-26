af09cb72fd35f5d161d478b9dab866e29ac44294
Ricardo Hochman <ricardohochman@hotmail.com>
2019-04-26 14:09:49 -0300

    Initial commit

---
+++ Amaro-Challenge-iOS/Amaro-Challenge-iOSTests/Amaro_Challenge_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Amaro_Challenge_iOSTests.swift
+//  Amaro-Challenge-iOSTests
+//
+//  Created by Ricardo Hochman on 26/04/19.
+//  Copyright © 2019 Ricardo Hochman. All rights reserved.
+//
+
+import XCTest
+@testable import Amaro_Challenge_iOS
+
+class Amaro_Challenge_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
